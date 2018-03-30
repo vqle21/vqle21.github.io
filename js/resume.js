4969984eff62b8e681b5c7880828b2614e851beb
huyle333 <huyle333@bu.edu>
2018-03-29 23:17:07 -0400

    Fixed the colors on the back-to-top class because it was always white, which made us to believe that the JavaScript wasn't working properly, but it actually was. White to gray. 2nd, we ended up moving the JavaScript into resume.js because it looks cleaner and we updated the format of the JavaScript.

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,22 @@
     target: '#sideNav'
   });
 
+  var offset = 100;
+  var duration = 300;
+  
+  $(window).scroll(function() {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.back-to-top').fadeIn(duration);
+    } 
+    else {
+      jQuery('.back-to-top').fadeOut(duration);
+    }
+  });
+
+  $('.back-to-top').click(function(event) {
+    event.preventDefault();
+    jQuery('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
+
 })(jQuery); // End of use strict
